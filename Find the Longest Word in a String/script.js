82fe985c92e51e7cde375787e5fc41474719dec6
Andriy Cherevko <cherevkin@gmail.com>
2018-02-11 18:01:41 +0200

    Added third way to find longest word in string (with metod .reduce)

--- Find the Longest Word in a String/script.js
+++ Find the Longest Word in a String/script.js
@@ -1,35 +1,52 @@
-//function findLongestWord(str) {
-//    str = str.split(' ');
-//    console.log(str);
-//    var lenght = 0;
-//    for (var i = 0; i < str.length; i++) {
-//        if (lenght < str[i].length) {
-//        lenght = str[i].length;
-//            }
-//        
-//    }
-//    console.log(lenght);
-//    return length;
-//}
-//
-//findLongestWord("The,  quick brown fox jumped over the lazy dog");
-//findLongestWord("May the force be with you");
-//findLongestWord("Google do a barrel roll");
-//findLongestWord("What is the average airspeed velocity of an unladen swallow");
-//findLongestWord("What if we try a super-long word such as otorhinolaryngology");
+function findLongestWord(str) {
+    str = str.split(' ');
+    console.log(str);
+    var lenght = 0;
+    for (var i = 0; i < str.length; i++) {
+        if (lenght < str[i].length) {
+        lenght = str[i].length;
+            }
+        
+    }
+    console.log(lenght);
+    return length;
+}
 
-//function findLongestWordSecondWay(str) { 
-//
-//    str = str.split(' ');
-//    str.sort(function(a, b){
-//        return b.length - a.length;
-//    });
-//    
-//    console.log(str[0].length);
-//}
-//
-//findLongestWordSecondWay("The,  quick brown fox jumped over the lazy dog");
-//findLongestWordSecondWay("May the force be with you");
-//findLongestWordSecondWay("Google do a barrel roll");
-//findLongestWordSecondWay("What is the average airspeed velocity of an unladen swallow");
-//findLongestWordSecondWay("What if we try a super-long word such as otorhinolaryngology");
+findLongestWord("The,  quick brown fox jumped over the lazy dog");
+findLongestWord("May the force be with you");
+findLongestWord("Google do a barrel roll");
+findLongestWord("What is the average airspeed velocity of an unladen swallow");
+findLongestWord("What if we try a super-long word such as otorhinolaryngology");
+
+function findLongestWordSecondWay(str) { 
+
+    str = str.split(' ');
+//    add custom function to sort array
+    str.sort(function(a, b){
+        return b.length - a.length;
+    });
+    
+    console.log(str[0].length);
+}
+
+findLongestWordSecondWay("The,  quick brown fox jumped over the lazy dog");
+findLongestWordSecondWay("May the force be with you");
+findLongestWordSecondWay("Google do a barrel roll");
+findLongestWordSecondWay("What is the average airspeed velocity of an unladen swallow");
+findLongestWordSecondWay("What if we try a super-long word such as otorhinolaryngology");
+
+
+
+
+function findLongestWordWithReduce(str) { 
+    var longestWord = str.split(' ')
+                            .reduce(function(longest, currentWord) {
+                                
+                               if(currentWord.length > longest.length) return currentWord;
+                                else return longest;
+                                
+                                }, "");
+    
+  return longestWord.length;  
+}
+var c = findLongestWordWithReduce("The,  quick brown fox jumped over the lazy dog");
