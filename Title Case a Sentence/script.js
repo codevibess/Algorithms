beb1502eb54ca01938992b46d6399921e74f7a8d
Andriy Cherevko <cherevkin@gmail.com>
2018-02-13 19:32:41 +0200

    Day 4 - Title Case Sentance

---
+++ Title Case a Sentence/script.js
@@ -0,0 +1,24 @@
+function titleCase(str) {
+  str = str.toLowerCase().split(' ');
+  for (var i = 0; i < str.length; i++) {
+    str[i] = str[i].charAt(0).toUpperCase() + str[i].slice(1); 
+  }
+  return str.join(' ');
+}
+titleCase("I'm a little tea pot");
+
+
+function titleCase(str) {
+  return str.toLowerCase().split(' ').map(function(word) {
+    return (word.charAt(0).toUpperCase() + word.slice(1));
+  }).join(' ');
+}
+titleCase("I'm a little tea pot");
+
+
+function titleCase(str) {
+  return str.toLowerCase().split(' ').map(function(word) {
+    return word.replace(word[0], word[0].toUpperCase());
+  }).join(' ');
+}
+titleCase("I'm a little tea pot");
