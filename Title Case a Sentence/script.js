38d04e4b0f2a21333d15b6d39e3d3142eb69ad36
Andriy Cherevko <cherevkin@gmail.com>
2018-02-14 12:02:48 +0200

    minor changes in Title Case algo

--- Title Case a Sentence/script.js
+++ Title Case a Sentence/script.js
@@ -1,24 +1,37 @@
 function titleCase(str) {
-  str = str.toLowerCase().split(' ');
-  for (var i = 0; i < str.length; i++) {
+    str = str.toLowerCase().split(' ');
+    
+    for (var i = 0; i < str.length; i++) {
     str[i] = str[i].charAt(0).toUpperCase() + str[i].slice(1); 
   }
-  return str.join(' ');
+   
+    return str.join(' ');
 }
-titleCase("I'm a little tea pot");
 
 
-function titleCase(str) {
-  return str.toLowerCase().split(' ').map(function(word) {
+var test = titleCase("I'm a little tea pot");
+
+
+
+function titleCaseWithMap(str) {
+  return str.toLowerCase()
+            .split(' ')                                              .map(function(word) {
     return (word.charAt(0).toUpperCase() + word.slice(1));
   }).join(' ');
 }
 titleCase("I'm a little tea pot");
 
 
-function titleCase(str) {
-  return str.toLowerCase().split(' ').map(function(word) {
+function titleCaseWithMapAndReplace(str) {
+  return str.toLowerCase()
+            .split(' ') 
+            .map(function(word) {
     return word.replace(word[0], word[0].toUpperCase());
   }).join(' ');
 }
 titleCase("I'm a little tea pot");
+
+testP(test);
+function testP(i) {
+    console.log(i);
+}
